852c064e19e3c5698d8668e41d48f36dc02a2da3
Ернур <Eroxa2580@mail.ru>
2023-03-30 17:55:23 +0600

    init

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { PrismaService } from 'src/prisma.service'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule } from '@nestjs/config/dist/config.module'
+import { ConfigService } from '@nestjs/config/dist/config.service'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { JwtStrategy } from './jwt.strategy'
+
+@Module({
+	controllers: [AuthController],
+	providers: [AuthService, PrismaService, JwtStrategy],
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	]
+})
+export class AuthModule {}
